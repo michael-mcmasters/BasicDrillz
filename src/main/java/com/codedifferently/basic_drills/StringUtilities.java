7130c463872294b35bb96c2909342728ab46f916
michael-mcmasters <jmcmasters411@gmail.com>
2020-10-28 13:08:57 -0400

    Make StringUtilities class pass all tests

--- src/main/java/com/codedifferently/basic_drills/StringUtilities.java
+++ src/main/java/com/codedifferently/basic_drills/StringUtilities.java
@@ -6,7 +6,7 @@ public class StringUtilities {
      * @return `Hello World` as a string
      */
     public static String getHelloWorld() {
-        return null;
+        return "Hello World";
     }
 
     /**
@@ -15,7 +15,7 @@ public class StringUtilities {
      * @return the concatenation of two strings, `firstSegment`, and `secondSegment`
      */
     public static String concatenation(String firstSegment, String secondSegment){
-        return null;
+        return firstSegment + secondSegment;
     }
 
     /**
@@ -24,7 +24,7 @@ public class StringUtilities {
      * @return the concatenation of an integer, `firstSegment`, and a String, `secondSegment`
      */
     public static String concatenation(int firstSegment, String secondSegment){
-        return null;
+        return firstSegment + secondSegment;
     }
 
     /**
@@ -32,7 +32,13 @@ public class StringUtilities {
      * @return the first 3 characters of `input`
      */
     public static String getPrefix(String input){
-        return null;
+         char firstChar = input.charAt(0);
+         char secondChar = input.charAt(1);
+         char thirdChar = input.charAt(2);
+         String firstStr = String.valueOf(firstChar);
+         String secondStr = String.valueOf(secondChar);
+         String thirdStr = String.valueOf(thirdChar);
+         return firstStr + secondStr + thirdStr;
     }
 
     /**
@@ -40,7 +46,13 @@ public class StringUtilities {
      * @return the last 3 characters of `input`
      */
     public static String getSuffix(String input){
-        return null;
+        String lastThreeChars = "";
+        for (int i = 0; i < input.length(); i++) {
+            if (i >= input.length() - 3) {
+                lastThreeChars += input.charAt(i);
+            }
+        }
+        return lastThreeChars;
     }
 
     /**
@@ -49,7 +61,7 @@ public class StringUtilities {
      * @return the equivalence of two strings, `inputValue` and `comparableValue`
      */
     public static Boolean compareTwoStrings(String inputValue, String comparableValue){
-        return null;
+        return inputValue.equals(comparableValue);
     }
 
     /**
@@ -57,7 +69,8 @@ public class StringUtilities {
      * @return the middle character of `inputValue`
      */
     public static Character getMiddleCharacter(String inputValue){
-        return null;
+        int index = inputValue.length() / 2;
+        return inputValue.charAt(index);
     }
 
     /**
@@ -65,7 +78,15 @@ public class StringUtilities {
      * @return the first sequence of characters
      */
     public static String getFirstWord(String spaceDelimitedString){
-        return null;
+        String firstWord = "";
+        for (char letter : spaceDelimitedString.toCharArray()) {
+            if (Character.isWhitespace(letter)) {
+                return firstWord;
+            }
+            firstWord += String.valueOf(letter);
+        }
+        // If code makes it here, there are no spaces in the string. There's only one word.
+        return firstWord;
     }
 
     /**
@@ -73,7 +94,25 @@ public class StringUtilities {
      * @return the second word of a string delimited by spaces.
      */
     public static String getSecondWord(String spaceDelimitedString){
-        return null;
+        String secondWord = "";
+        boolean loopedOverFirstSpace = false;
+        for (char letter : spaceDelimitedString.toCharArray()) {
+            if (loopedOverFirstSpace) {
+                if (Character.isWhitespace(letter)) {
+                    break; // breaks out of loop if a 3rd word is found.
+                }
+                secondWord += String.valueOf(letter);
+            }
+            if (!loopedOverFirstSpace && Character.isWhitespace(letter)) {
+                loopedOverFirstSpace = true;
+            }
+        }
+
+        if (!loopedOverFirstSpace) {
+            throw new IllegalArgumentException("Could not find second word in string. Are you sure it was more than one word?");
+        }
+
+        return secondWord;
     }
 
     /**
@@ -81,6 +120,10 @@ public class StringUtilities {
      * @return an identical string with characters in reverse order.
      */
     public static String reverse(String stringToReverse){
-        return null;
+        String reversedStr = "";
+        for (int i = stringToReverse.length() - 1; i >= 0; i--) {
+            reversedStr += stringToReverse.charAt(i);
+        }
+        return reversedStr;
     }
 }
